4c70a1e9988b8aa9c3e9f14f6a780bb9182eb8a4
TibshoOT <thibaut.deloffre@gmail.com>
2010-11-26 12:03:28 +0100

    [Add] first ktrace demo code

---
+++ my_ktrace.c
@@ -0,0 +1,14 @@
+#include "my_ktrace.h"
+#include "my_ktrace_utils.h"
+#include "my_ktrace_optparse.h"
+
+int		main(int argc, char **argv)
+{
+	struct k_inf	 k_info;
+
+  k_info = optparser(argc, argv); 
+  check_tracefile(k_info);
+	do_ktrace_syscalls(k_info);
+
+  return (0);
+}
---
+++ my_ktrace_optparse.h
@@ -0,0 +1,6 @@
+#ifndef __MY_KTRACE_OPTPARSE__
+# define __MY_KTRACE_OPTPARSE__
+
+struct k_inf  optparser(int, char **);
+
+#endif
