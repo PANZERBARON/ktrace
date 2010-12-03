cdd2de006704fb9f20e571712e713e2b86201bed
TibshoOT <thibaut.deloffre@gmail.com>
2010-12-03 03:01:15 +0100

    [Mod] Check SIGSYS signal handling in case of 'no ktrace compliant' kernel

--- my_ktrace.c
+++ my_ktrace.c
@@ -7,6 +7,7 @@ int   main(int argc, char **argv)
   struct k_inf  k_info;
 
   k_info = optparser(argc, argv);
+  signal(SIGSYS, no_kernel_compliance);
   check_tracefile(k_info);
   do_ktrace_syscalls(k_info);
 
--- my_ktrace_utils.h
+++ my_ktrace_utils.h
@@ -1,10 +1,15 @@
 #ifndef __MY_KTRACE_UTILS__
 # define __MY_KTRACE_UTILS__
 
+#include <signal.h>
+
+#define KERNEL_COMPLIANCE_MSG "Your kernel hasn't been compiled to ktrace support."
+
 struct  k_inf  init_infos_struct(void);
 void           check_tracefile(struct k_inf);
 void           do_ktrace_syscalls(struct k_inf);
 void           usage_error(void);
 int            ck_pid_mutual_valid(char *);
+void           no_kernel_compliance(int);
 
 #endif
