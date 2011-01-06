5be28660f814d9516900a6b694badccb7006ed78
TibshoOT <thibaut.deloffre@gmail.com>
2011-01-06 14:36:12 +0100

    [Mod] check defitions in source files

--- my_ktrace.c
+++ my_ktrace.c
@@ -2,11 +2,11 @@
 #include "my_ktrace_utils.h"
 #include "my_ktrace_optparse.h"
 
-int   main(int argc, char **argv)
+int   main(int ac, char **av)
 {
   struct k_inf  k_info;
 
-  k_info = optparser(argc, argv);
+  k_info = optparser(ac, av);
   signal(SIGSYS, no_kernel_compliance);
   check_tracefile(k_info);
   do_ktrace_syscalls(k_info);
